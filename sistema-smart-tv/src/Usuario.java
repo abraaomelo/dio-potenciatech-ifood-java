2a5bd09491d9566501ec6662534e6c6e58b5de1c
Melo Abraao <abraao.st.melo@gmail.com>
2022-11-17 00:06:04 -0300

    challenge1 updated

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,11 +1,25 @@
+import java.util.*;
 public class Usuario {
     public static void main(String[] args) throws Exception {
-        
-        SmartTv smartTv = new SmartTv();
+       int A,B;
+       Scanner leitor = new Scanner(System.in);
 
-        smartTv.ligar();
-        System.out.println("TV Ligada: " + smartTv.ligada);
-        System.out.println("Canal Atual: " + smartTv.canal);
-        System.out.println("Volume: " + smartTv.volume);
-    }
+       
+       System.out.println("Digite o primeiro valor");
+       A = leitor.nextInt();
+
+       System.out.println("Digite o segundo valor");
+       B = leitor.nextInt();
+       
+       if (A==B){
+        System.out.println("Sao iguais!");
+       }else{
+        System.out.println("Nao sao iguais!");
+       }
+    
+        
+       
+
+       
+}
 }
